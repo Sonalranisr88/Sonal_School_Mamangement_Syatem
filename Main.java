1f19af094e91876807fd5ffba08e322741a77f72
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-06-13 09:53:58 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 import java.applet.*;
 
 
-public class sms extends Applet implements ActionListener {
+public class Main extends Applet implements ActionListener {
   String msg = "*****WELCOME TO SONAL SCHOOL MANAGEMENT*****";
   String msg2 = "";
 
